76ecd7b74725d6b9b49c676dec984bd9666fca95
Saidorifkhuja Yuldoshkhujaev <yoldoshxojayevsaidorifxoja@gmail.com>
2025-04-07 16:45:08 +0500

    chinoz

--- worker/models.py
+++ worker/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Worker(models.Model):
     uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-    name = models.CharField(max_length=250)
-    photo = models.ImageField(upload_to='worker/')
-    description = models.TextField(null=True, blank=True)
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='worker/')
+    body = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
